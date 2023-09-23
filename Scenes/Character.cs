0450871f38b6724e88911aff692045ff5c811d70
Alex Silvestri <alexsilvestri.16@gmail.com>
2023-09-23 14:39:20 +0200

    more tests

---
+++ Scenes/Character.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Character : KinematicBody2D
+{
+
+	public override void _Ready()
+	{
+	}
+
+  	public override void _Process(float delta)
+  	{
+	  
+  	}
+}
