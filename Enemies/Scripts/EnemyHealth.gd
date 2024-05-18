f3a661ca04c2a8834a013cbc569c2d52290f6b55
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-05-18 17:55:55 +0200

    Add health to enemies

---
+++ Enemies/Scripts/EnemyHealth.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var maxHealth: int
+@export var enemyController: EnemyController
+@export var label: Label
+var currentHealth: int = 0
+
+func _ready():
+	HealthStartup()
+
+func HealthStartup():
+	HealthUpdate(maxHealth)
+
+func HealthUpdate(valueChange: int):
+	currentHealth = clamp(currentHealth + valueChange, 0, maxHealth)
+	label.text = str(currentHealth, "/", maxHealth)
+	if (currentHealth <= 0):
+		enemyController.get_parent().remove_child(enemyController)
