bb5e370ff63f3e4dce08d6e455c42ec3cb89b41a
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-05-25 15:16:38 +0200

    End Map Area done

---
+++ MapShuffler/EndMapArea.gd
@@ -0,0 +1,10 @@
+class_name EndMapArea
+extends Area2D
+
+func _on_body_entered(body):
+	if (body is PlayerCharacter):
+		GoToNextMap()
+
+func GoToNextMap():
+	var sceneMaster: SceneMaster = get_tree().root.get_child(0)
+	sceneMaster.sceneSelector.ShuffleScene()
