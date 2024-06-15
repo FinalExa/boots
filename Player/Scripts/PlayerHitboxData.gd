6928458943cb8cf3e8abae5933acfa6f253f6841
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-06-15 14:46:16 +0200

    Removed list from attack hitboxes

--- Player/Scripts/PlayerHitboxData.gd
+++ Player/Scripts/PlayerHitboxData.gd
@@ -25,16 +25,7 @@ enum PlayerHitboxType
 @export var clashLossOnImpact: float
 @export var clashSound: AudioStreamPlayer
 @export var impactTypeIndicator: String
-var enemiesHit: Array[EnemyController]
 var damageEnabled: bool
-
-func _on_body_entered(body):
-	if (damageEnabled):
-		CheckCollision(body, playerHitboxType)
-
-func _on_area_entered(area):
-	if (damageEnabled):
-		CheckArea(area, playerHitboxType)
 
 func CheckCollision(body, type: PlayerHitboxType):
 	if (body is EnemyController):
@@ -67,11 +58,8 @@ func Clash(enemyController: EnemyController):
 	DealDamage(enemyController, clashDamage, clashRepelDistance, playerMovements.currentDirection, clashRepelTime, clashLossOnImpact, true)
 
 func DealDamage(enemyController: EnemyController, damageDealt: int, repelDist: float, direction: Vector2, time: float, speedLoss: float, clash: bool):
-	if (!enemyController.damageImmunity && !enemiesHit.has(enemyController)):
-		enemiesHit.push_back(enemyController)
+	if (!enemyController.damageImmunity):
 		enemyController.ReceiveDamage(damageDealt, repelDist, direction, time)
-		if (enemyController == null):
-			enemiesHit.erase(enemyController)
 		if (clash):
 			clashSound.play()
 			emit_signal("hitClash")
@@ -99,13 +87,11 @@ func CreateImpactTypeIndicator(clash: bool, currentPosition: Vector2):
 
 func TurnOffDamage():
 	damageEnabled = false
-	enemiesHit.clear()
 
-func _on_body_exited(body):
-	if (body is EnemyController && enemiesHit.has(body)):
-		enemiesHit.erase(body)
+func _on_body_entered(body):
+	if (damageEnabled):
+		CheckCollision(body, playerHitboxType)
 
-func _on_area_exited(area):
-	if (area.is_in_group(clashGroupName)):
-		if (area is AttackHitbox && enemiesHit.has(area.get_parent().characterRef)):
-			enemiesHit.erase(area.get_parent().characterRef)
+func _on_area_entered(area):
+	if (damageEnabled):
+		CheckArea(area, playerHitboxType)
