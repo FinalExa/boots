a619e98eebc84b874d16edb6ffca09c02b329a78
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-09-24 17:43:23 +0200

    Export v_0.0.0.27

--- Player/Scripts/PlayerInputs.gd
+++ Player/Scripts/PlayerInputs.gd
@@ -27,7 +27,6 @@ func GetInputs():
 		GetAbility1Input()
 		GetAbility2Input()
 		GetInteractionInput()
-		WinMap()
 		if (Input.is_action_just_pressed("Reload")):
 			get_tree().reload_current_scene()
 		if (Input.is_action_just_pressed("Close")):
