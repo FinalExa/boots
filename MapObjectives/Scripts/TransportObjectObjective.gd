3f423c95481691f6bb4dc7080d58ac165747622a
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-10-08 17:27:44 +0200

    Continued TransportObjectObjective

--- MapObjectives/Scripts/TransportObjectObjective.gd
+++ MapObjectives/Scripts/TransportObjectObjective.gd
@@ -3,10 +3,16 @@ extends MapObjective
 
 @export var transportObjectSpawner: ObjectSpawner
 @export var transportDestinations: Array[TransportDestination]
+var transportObject: TransportObject
 
 func ReadyOperations():
+	GenerateTransportObject()
 	RegisterDestinations()
 
 func RegisterDestinations():
 	for i in transportDestinations.size():
 		transportDestinations[i].mapObjective = self
+
+func GenerateTransportObject():
+	if (transportObject == null && transportObjectSpawner != null):
+		transportObject = transportObjectSpawner.SpawnObject()
