466fa2794a2aedf846582a297522dd93c777c95b
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-10-08 18:34:03 +0200

    Created TransportObject

--- MapObjectives/ObjectiveObjects/Scripts/TransportObject.gd
+++ MapObjectives/ObjectiveObjects/Scripts/TransportObject.gd
@@ -1,3 +1,16 @@
 class_name TransportObject
 extends Node2D
 
+var playerIsIn: bool
+var playerRef: PlayerCharacter
+var isAttachedToPlayer: bool
+
+func _on_area_2d_body_entered(body):
+	if (body is PlayerCharacter && !playerIsIn):
+		playerIsIn = true
+		playerRef = body
+
+func _on_area_2d_body_exited(body):
+	if (!isAttachedToPlayer && body is PlayerCharacter && playerIsIn):
+		playerIsIn = false
+		playerRef = null
