fa50dc4718fc60727a15a325ef8721b747ad06b4
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-10-11 17:20:57 +0200

    Added everything except enemy spawn

--- MapObjectives/ObjectiveObjects/Scripts/TransportDestination.gd
+++ MapObjectives/ObjectiveObjects/Scripts/TransportDestination.gd
@@ -1,4 +1,24 @@
 class_name TransportDestination
 extends Node2D
 
-var mapObjective: MapObjective
+var mapObjective: TransportObjectObjective
+var playerIsIn: bool
+var playerRef: PlayerCharacter
+var completed: bool
+
+func SetCompleted(transportObject: TransportObject):
+	completed = true
+	transportObject.DetachFromPlayer()
+	mapObjective.SetDestinationCompleted(self)
+	
+func _on_area_2d_body_entered(body):
+	if (body is PlayerCharacter && !playerIsIn):
+		playerIsIn = true
+		playerRef = body
+		if (!completed && playerRef.followItem.get_child(0) is TransportObject):
+			SetCompleted(playerRef.followItem.get_child(0))
+
+func _on_area_2d_body_exited(body):
+	if (body is PlayerCharacter && playerIsIn):
+		playerIsIn = false
+		playerRef = null
