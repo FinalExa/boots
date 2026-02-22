170b73b8c277b2f0b0fd1408d1625391ff445d5a
Exa <alexsilvestri.16@gmail.com>
2026-02-22 14:30:38 +0100

    Speed bar changes color

--- Player/HUD/Scripts/SpeedValue.gd
+++ Player/HUD/Scripts/SpeedValue.gd
@@ -1,19 +1,24 @@
 extends TextureProgressBar
 
 @export var playerMovements: PlayerMovements
+@export var playerSpeedThresholds: PlayerSpeedThresholds
 @export var accelerationLabel: Label
+@export var barColors: Array[Color]
 var savedSpeed: int
 var savedAcceleration: int
 
 func _ready():
 	accelerationLabel.text = str(0)
 	max_value = playerMovements.maxSpeed
+	tint_progress = barColors[0]
 
 func _process(_delta):
-	SetText()
+	SetBar()
 
-func SetText():
+func SetBar():
 	if (savedSpeed != playerMovements.currentSpeed):
+		if (tint_progress != barColors[playerSpeedThresholds.speedIndex]):
+			tint_progress = barColors[playerSpeedThresholds.speedIndex]
 		savedSpeed = int(playerMovements.currentSpeed)
 		value = savedSpeed
 		var valueToShow: int
