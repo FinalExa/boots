e67bc99776de38aaeb31a173df440787ec942644
Exa <alexsilvestri.16@gmail.com>
2026-03-02 20:12:55 +0100

    Spawner direction fixed

--- Spawners/Scripts/ObjectSpawner.gd
+++ Spawners/Scripts/ObjectSpawner.gd
@@ -2,7 +2,6 @@ class_name ObjectSpawner
 extends Node2D
 
 @export var objectPath: String
-@export var setAsChild: bool
 
 func SpawnObject():
 	if (self.is_inside_tree() && objectPath != ""):
@@ -14,19 +13,16 @@ func SpawnObject():
 func CreateAndReturnObject(objectToCreate: String, objectPositionRef: Node2D):
 	var obj_scene = load(objectToCreate)
 	var obj = obj_scene.instantiate()
+	obj.global_position = objectPositionRef.global_position
+	obj.global_rotation = objectPositionRef.global_rotation
 	call_deferred("AddObjectToScene", obj, objectPositionRef)
 	if (obj is EnemyController): obj.SetSpawnerRef(self)
 	return obj
 
 func AddObjectToScene(object, objectPositionRef: Node2D):
-	if (!setAsChild):
-		var sceneMaster: SceneMaster = get_tree().root.get_child(0)
-		if (sceneMaster != null):
-			sceneMaster.sceneSelector.currentScene.add_child(object)
-	else:
-		self.add_child(object)
-	object.global_position = objectPositionRef.global_position
-	object.global_rotation = objectPositionRef.global_rotation
+	var sceneMaster: SceneMaster = get_tree().root.get_child(0)
+	if (sceneMaster != null):
+		sceneMaster.sceneSelector.currentScene.add_child(object)
 
 func ReceivedCallFromDeletedSpawnedObject(_object):
 	pass
