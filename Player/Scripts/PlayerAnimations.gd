29fcd40c89c9fbf63874c7c60cda732abf939558
Exa <alexsilvestri.16@gmail.com>
2026-06-22 22:28:56 +0200

    Added scene for player sprite, added run_up animations

--- Player/Scripts/PlayerAnimations.gd
+++ Player/Scripts/PlayerAnimations.gd
@@ -1,10 +1,10 @@
 extends AnimatedSprite2D
 
-@export var isHub: bool
 @export var playerMovements: PlayerMovements
 @export var playerSpeedThresholds: PlayerSpeedThresholds
-@export var playerHubMovement: PlayerHubMovement
 @export var stagesFrameSpeed: Array[float]
+@export var isHub: bool
+@export var playerHubMovement: PlayerHubMovement
 
 func _process(_delta):
 	SelectCurrentAnimation()
@@ -17,7 +17,7 @@ func SelectCurrentAnimation():
 
 func NormalAnimations():
 	if (playerMovements.currentSpeed > 0):
-		play("run_front")
+		SetCorrectAnimation(playerMovements.currentDirection)
 		speed_scale = stagesFrameSpeed[playerSpeedThresholds.speedIndex]
 		return
 	play("idle")
@@ -25,8 +25,14 @@ func NormalAnimations():
 
 func HubAnimations():
 	if (playerHubMovement.currentSpeed > 0):
-		play("run_front")
+		SetCorrectAnimation(playerHubMovement.playerHubRef.playerInputs.movementInput)
 		speed_scale = 1
 		return
 	play("idle")
 	return
+
+func SetCorrectAnimation(direction: Vector2):
+	if (direction.y >= 0):
+		play("run_down")
+		return
+	play("run_up")
