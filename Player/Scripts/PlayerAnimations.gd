fd03fbec15fda311d670547bbefad75787a63150
Exa <alexsilvestri.16@gmail.com>
2026-06-28 14:19:05 +0200

    Added four side animations for the player

--- Player/Scripts/PlayerAnimations.gd
+++ Player/Scripts/PlayerAnimations.gd
@@ -32,7 +32,26 @@ func HubAnimations():
 	return
 
 func SetCorrectAnimation(direction: Vector2):
-	if (direction.y >= 0):
+	if (abs(direction.x) > 0 && abs(direction.y) > 0):
+		if (abs(direction.x) >= abs(direction.y)):
+			HorizontalAnimations(direction.x)
+			return
+		VerticalAnimations(direction.y)
+	if (direction.y == 0 && direction.x != 0):
+		HorizontalAnimations(direction.x)
+		return
+	if (direction.x == 0 && direction.y != 0):
+		VerticalAnimations(direction.y)
+		return
+
+func HorizontalAnimations(xValue: float):
+	if (xValue >= 0):
+		play("run_right")
+		return
+	play("run_left")
+
+func VerticalAnimations(yValue: float):
+	if (yValue >= 0):
 		play("run_down")
 		return
 	play("run_up")
